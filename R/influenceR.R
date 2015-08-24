c13cc97df06df13db784070aaeff8f8ca4c246b2
Simon Jacobs <sdjacobs@uchicago.edu>
2015-08-24 14:34:17 -0500

    updated version and documentation

---
+++ R/influenceR.R
@@ -0,0 +1,23 @@
+#' influenceR: Software tools to quantify structural importance of nodes in a network.
+#'
+#' The influenceR package enhances igraph by including more functions to
+#' quantify the structural importance of nodes in a network. Algorithms include
+#' Betweenness Centrality, Bridging, Constraint Index, Effective Network Size,
+#' and Key Players. Currently, algorithms are only guaranteed to work on 
+#' undirected graphs; work on directed graphs is in progress.
+#' 
+#' In addition to igraph, this package makes use of the SNAP framework for a
+#' high-performance graph data structure and an OpenMP-parallelized
+#' implementation of Betweenness Centrality. See
+#' \url{http://snap-graph.sourceforge.net}
+#' 
+#' @section Funding:
+#' Development of this software package was supported by NIH grant R01 DA033875.
+#' 
+#' @section References:
+#' The website and source code is located at \url{http://github.com/rcc-uchicago/influenceR}.
+#'
+#' @docType package
+#' @name influenceR
+NULL
+#> NULL
